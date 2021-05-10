a8b4b41e4c858bd313b4a6cf35f836c23f8e23df
potato-j <2087263877@qq.com>
2021-05-10 16:03:51 +0800

    java of first upload

---
+++ src/text/BeanUtilsTest.java
@@ -0,0 +1,30 @@
+package text;
+
+import domain.User;
+import jdk.jfr.StackTrace;
+import org.apache.commons.beanutils.BeanUtils;
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class BeanUtilsTest {
+
+    @Test
+    public void test(){
+        User user = new User();
+
+        try {
+            BeanUtils.setProperty(user,"hehe","male");
+            System.out.println(user);
+
+            String gender = BeanUtils.getProperty(user,"hehe");
+            System.out.println(gender);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/text/testLogin.java
@@ -0,0 +1,20 @@
+package text;
+
+import dao.UserDao;
+import domain.User;
+import org.junit.Test;
+
+public class testLogin {
+
+    @Test
+    public void testLogin(){
+        User loginuser = new User();
+        loginuser.setUsername("superbaby");
+        loginuser.setPassword("123");
+
+        UserDao dao = new UserDao();
+        User user = dao.login(loginuser);
+
+        System.out.println(user);
+    }
+}
